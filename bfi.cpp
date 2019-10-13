9a28c9aedf27e352fcc43c6f235c87213857c7ce
Peter Züger <zueger.peter@icloud.com>
2019-10-13 11:49:05 +0200

    added license to header comment

--- bfi.cpp
+++ bfi.cpp
@@ -3,6 +3,21 @@
  * @author Peter Züger
  * @date   06.07.2019
  * @brief  BrainFuck Interpreter
+ *
+ * This file is part of Brainfuck (https://gitlab.com/peterzuger/BrainFuck).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <algorithm>
 #include <array>
